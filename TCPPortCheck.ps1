0e9218131e141b293e3d7d7d581ea79f64c20e92
Rei Iga <31835726+Igaigasuru@users.noreply.github.com>
2019-12-12 20:41:11 +0900

    Update TCPPortCheck.ps1

--- TCPPortCheck.ps1
+++ TCPPortCheck.ps1
@@ -1,6 +1,13 @@
 $ip1="<Primary Server IP Address>"
 $ip2="<Secondary Server IP Address>"
 
+date /t | Select-String $d
+time /t | Select-String $t
+echo "$d $t"
+
+hostname | Select-String $name
+echo "My hostname is $name"
+
 $ret = ipconfig | Select-String $ip1
 if ( $ret -eq "" )
 {
@@ -15,51 +22,9 @@ else{
 echo "Other IP is $targetip"
 
 Test-NetConnection -ComputerName $targetip -Port 29001
-if ( $? -eq $true ){
-  echo "Port 29001(TCP): Opend"
-}
-else{
-  echo "Port 29001(TCP): Closed. Please open this port!"
-}  
-
 Test-NetConnection -ComputerName $targetip -Port 29002
-if ( $? -eq $true ){
-  echo "Port 29002(TCP): Opend."
-}
-else{
-  echo "Port 29002(TCP): Closed. Please open this port!"
-}  
-
 Test-NetConnection -ComputerName $targetip -Port 29003
-if ( $? -eq $true ){
-  echo "Port 29003(TCP): Opend."
-}
-else{
-  echo "Port 29003(TCP): Closed. Please open this port!"
-}  
-
 Test-NetConnection -ComputerName $targetip -Port 29004
-if ( $? -eq $true ){
-  echo "Port 29004(TCP): Opend."
-}
-else{
-  echo "Port 29004(TCP): Closed. Please open this port!"
-}  
-
 Test-NetConnection -ComputerName $targetip -Port 29005
-if ( $? -eq $true ){
-  echo "Port 29005(TCP): Opend."
-}
-else{
-  echo "Port 29005(TCP): Closed. Please open this port!"
-}  
-
 Test-NetConnection -ComputerName $targetip -Port 29007
-if ( $? -eq $true ){
-  echo "Port 29007(TCP): Opend."
-}
-else{
-  echo "Port 29007(TCP): Closed. Please open this port!"
-}
-
 Read-Host "Press Enter to close this window..."
