edbb9cb0b4a6327944927cc5c027bdd95d2d7705
yogata <42662036+oga-clp@users.noreply.github.com>
2020-02-04 17:58:03 +0900

    Add files via upload

---
+++ TCPPortCheck-Socket.ps1
@@ -0,0 +1,6 @@
+$tcpCl = New-Object Sytem.Net.Sockets.tcpClient
+$tcpCl.connect("192.168.0.100", 80)
+# If TCP connection cannot be established, the "Connect" error is shown.
+$tcpCl.connected
+# True or False
+$tcpCl.close()
