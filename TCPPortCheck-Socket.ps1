aa6c77a470d757d3a21e9253cd3bc3aa169e6ea1
yogata <42662036+oga-clp@users.noreply.github.com>
2020-02-04 17:59:48 +0900

    Update TCPPortCheck-Socket.ps1

--- TCPPortCheck-Socket.ps1
+++ TCPPortCheck-Socket.ps1
@@ -1,4 +1,5 @@
 $tcpCl = New-Object Sytem.Net.Sockets.tcpClient
+# $tcpCl.connect("IP address", port)
 $tcpCl.connect("192.168.0.100", 80)
 # If TCP connection cannot be established, the "Connect" error is shown.
 $tcpCl.connected
