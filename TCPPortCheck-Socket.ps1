5333ba0fb8d1573ef08dcfbc2a0eb07134132235
yogata <42662036+oga-clp@users.noreply.github.com>
2020-02-06 11:32:30 +0900

    Delete TCPPortCheck-Socket.ps1

--- TCPPortCheck-Socket.ps1
+++
@@ -1,7 +0,0 @@
-$tcpCl = New-Object Sytem.Net.Sockets.tcpClient
-# $tcpCl.connect("IP address", port)
-$tcpCl.connect("192.168.0.100", 80)
-# If TCP connection cannot be established, the "Connect" error is shown.
-$tcpCl.connected
-# True or False
-$tcpCl.close()
